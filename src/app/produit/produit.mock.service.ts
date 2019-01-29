f9ff87c6aae1da4e0220cb8987b9974d231343bd
kkhenissi <kkhenissi@yahoo.fr>
2019-01-29 23:35:54 +0100

    sans mook

--- src/app/produit/produit.mock.service.ts
+++ src/app/produit/produit.mock.service.ts
@@ -7,15 +7,15 @@ export class ProduitMockService implements CrudService {
 
     private Produits: Produit[] = [];
     constructor() {
-        const p1: Produit = new Produit(1, 'Livre', 50, 20);
-        const p2: Produit = new Produit(2, 'Cahier', 60, 10);
-        const p3: Produit = new Produit(3, 'Stylo', 20, 240);
-        const p4: Produit = new Produit(4, 'Ram de papier', 14, 230);
+        // const p1: Produit = new Produit(1, 'Livre', 50, 20);
+        // const p2: Produit = new Produit(2, 'Cahier', 60, 10);
+        // const p3: Produit = new Produit(3, 'Stylo', 20, 240);
+        // const p4: Produit = new Produit(4, 'Ram de papier', 14, 230);
 
-        this.Produits.push(p1);
-        this.Produits.push(p2);
-        this.Produits.push(p3);
-        this.Produits.push(p4);
+        // this.Produits.push(p1);
+        // this.Produits.push(p2);
+        // this.Produits.push(p3);
+        // this.Produits.push(p4);
     }
 
     getAll(): Observable<any> {
